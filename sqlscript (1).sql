f863c35c2829a5f387730a4ab53d817b5d3aa37a
surbbahl <75295772+surbbahl@users.noreply.github.com>
2020-12-11 22:56:04 +0530

    Add files via upload

---
+++ sqlscript (1).sql
@@ -0,0 +1,264 @@
+REM   Script: SQL_activity1
+REM   SQL activity1
+
+create table salesman( 
+Salesman_id number;
+
+Salesman_name varchar2(32); 
+
+
+Salesman_city varchar2(32); 
+
+
+commission Number; 
+
+
+)
+
+
+create table salesman( 
+Salesman_id number, 
+Salesman_name varchar2(32), 
+Salesman_city varchar2(32), 
+commission Number 
+);
+
+create table salesman( 
+Salesman_id number, 
+Salesman_name varchar2(32), 
+Salesman_city varchar2(32), 
+commission Number 
+);
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12);
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+Select 1 from Dual;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+Select 1 from Dual;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+Insert ALL 
+into salesman values (5005,'PA','London',11) 
+into salesamn values(5006,'Mclyon', 'Paris',14) 
+into salesman values (5007,'PaulAdam','Rome',13) 
+into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert into salesman values (5002,'Nail Knite','Paros',13);
+
+Insert ALL 
+    into salesman values (5005,'PA','London',11) 
+    into salesamn values(5006,'Mclyon', 'Paris',14) 
+    into salesman values (5007,'PaulAdam','Rome',13) 
+    into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+Insert ALL 
+    into salesman values (5005,'PA','London',11) 
+    into salesamn values(5006,'Mclyon', 'Paris',14) 
+    into salesman values (5007,'PaulAdam','Rome',13) 
+    into salesman values(5003,'Lauson Hen','SanJose',12) 
+ 
+Select 1 from DUAL;
+
+insert into salesman values(5002,'NailKnite','Paris',13);
+
+insert into salesman values(5002,'NailKnite','Paris',13);
+
+insert into salesman values(5005,'BB','London',14);
+
+insert into salesman values(5006,'CC','US',16);
+
+insert into salesman values(5007,'DD','UK',17);
+
+INSERT ALL 
+into salesman values(5002,'NailKnite','Paris',13);
+
+into salesman values(5005,'BB','London',14); 
+
+
+into salesman values(5006,'CC','US',16); 
+
+
+into salesman values(5007,'DD','UK',17);
+
+
+INSERT ALL 
+into salesman values(5002,'NailKnite','Paris',13) 
+into salesman values(5005,'BB','London',14) 
+into salesman values(5006,'CC','US',16) 
+into salesman values(5007,'DD','UK',17) 
+ 
+select * from DUAL;
+
+INSERT ALL 
+into salesman values(5002,'NailKnite','Paris',13) 
+into salesman values(5005,'BB','London',14) 
+into salesman values(5006,'CC','US',16) 
+into salesman values(5007,'DD','UK',17) 
+ 
+select * from DUAL;
+
+select salesman_id,salesman_city from salesman;
+
+select salesman_id,salesman_city from salesman;
+
+select salesman_id,salesman_city from salesman;
+
+select * from salesman where salesman_city='Paris';
+
+select * from salesman where salesman_city='Paris';
+
+select * from salesman where salesman_city='Paris';
+
+select salesman_id,commission from salesman where  
+salesman_name='NailKnite';
+
+alter table salesman add grade int;
+
+update salesman set grade =100;
+
+update salesman set grade =100;
+
+select * from salesman;
+
+select * from salesman;
+
+update salesman set grade=200  
+where salesman_city='Paris';
+
+select* from salesman;
+
+select* from salesman;
+
+select* from salesman;
+
+select* from salesman;
+
+select* from salesman;
+
+select* from salesman;
+
+select* from salesman;
+
+update salesman set Grade=300 
+where salesman_name='BB';
+
+update salesman set Grade=300 
+where salesman_name='BB';
+
+update salesman set Grade=300 
+where salesman_name='BB';
+
+select * from salesman;
+
+select * from salesman 
+ 
+update salesman set  
+salesman_name='KK' 
+where salesman_name='CC';
+
+select * from salesman 
+ 
+update salesman set  
+salesman_name='KK' 
+where salesman_name='CC';
+
+select * from salesman 
+ 
+update salesman set  
+salesman_name='KK' 
+where salesman_name='CC';
+
+update salesman set  
+salesman_name='KK' 
+where salesman_name='CC';
+
