ecb3f56fa67e79e91f19622586bcf804c54254a1
surbbahl <75295772+surbbahl@users.noreply.github.com>
2021-07-16 20:37:15 +0530

    Add files via upload

--- Project_1.java
+++ Project_1.java
@@ -1,36 +1,5 @@
-package Project_1;
+package Project_RestAssured;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 public class Project_1 {
-	WebDriver driver;
-	@BeforeMethod
-	
-	public void Website_title()
-	{
-		driver=new FirefoxDriver();
-		driver.get("https://alchemy.hguy.co/crm");
-	}
-	
-	@Test
-	public void title_match()
-	{
-		String Title =driver.getTitle();
-		System.out.println("Title="+Title);
-		boolean bul=driver.getTitle().equalsIgnoreCase("SuiteCRM");
-		if (bul==true)
-				{
-			driver.close();
-				}
-	}
-	@AfterMethod
-	public void Website_close()
-	{
-		System.out.println("Closing the test ");
-	}
+
 }
